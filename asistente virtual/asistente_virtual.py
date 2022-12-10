3e5c140786e4a8de878cb262752ab6f83329d750
EliabParra <117409776+EliabParra@users.noreply.github.com>
2022-12-10 15:26:01 -0400

    Create asistente_virtual.py

---
+++ asistente virtual/asistente_virtual.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+from time import *
+import wikipedia
+from os import system
+
+name = 'alexa'
+listener = sr.Recognizer()
+key = ''
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print('Escuchando...')
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice, language="es")
+            rec = rec.lower()
+            if name in rec:
+                rec = rec.replace(name, '')
+            else:
+                talk('Vuelve a intentarlo, no reconozco: '+rec)
+    except:
+        pass
+    return rec
+
+def run():
+    rec = listen()
+    if 'reproduce' in rec:
+        music = rec.replace('reproduce', 'Reproduciendo')
+        talk(music)
+        pywhatkit.playonyt(music)
+    elif 'hora' in rec:
+        hora = strftime('%H:%M %p')
+        talk('Son las '+hora)
+    elif 'busca' in rec:
+        order = rec.replace('busca', '')
+        pywhatkit.search(order)
+    elif 'abre' in rec:
+        app = rec.replace('abre', '')
+        system(app)
+        talk('abriendo'+app)
+    else:
+        talk('Vuelve a intentarlo, no reconozco: '+rec)
+
+run()
