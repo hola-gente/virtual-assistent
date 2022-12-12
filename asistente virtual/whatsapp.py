f088e1a663f9cd9dc033ac6730c5e1c7c434f4c1
EliabParra <117409776+EliabParra@users.noreply.github.com>
2022-12-12 15:41:39 -0400

    Update whatsapp.py

--- asistente virtual/whatsapp.py
+++ asistente virtual/whatsapp.py
@@ -1,2 +1,8 @@
 import webbrowser
+import pyautogui as at
+import time
 
+def enviar_msj(contact, message):
+    webbrowser.open(f"https://web.whatsapp.com/send?phone={contact}&text={message}")
+    time.sleep(8)
+    at.press('enter')
