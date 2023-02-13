c7b270feb41d55223c5c8fdfed9ce7483c4e877e
EliabParra <117409776+EliabParra@users.noreply.github.com>
2023-02-13 08:56:45 -0400

    Update whatsapp.py

--- asistente virtual/whatsapp.py
+++ asistente virtual/whatsapp.py
@@ -6,3 +6,5 @@ def enviar_msj(contact, message):
     webbrowser.open(f"https://web.whatsapp.com/send?phone={contact}&text={message}")
     time.sleep(8)
     at.press('enter')
+
+enviar_msj('Mami', 'hola')
